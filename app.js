9935d8b9c7a274cee83860b23b914d257835432e
Noah2211 <noah.decroix3@gmail.com>
2021-11-30 11:44:36 +0100

    exercice done

--- app.js
+++ app.js
@@ -31,7 +31,19 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
+}
+
+for (let i = 0; i < voitures.collection.length; i+=4) {
+    citroen.ouvrirPorte();
+    peugeot.ouvrirPorte();
+    ford.ouvrirPorte();
+    let spanCreate = document.createElement("span");
+    spanCreate.innerHTML = "La couleur de la citroen est: " + citroen.color + '<br>'
+    + "La couleur de la peugot est: " + peugeot.color + '<br>' +
+        "La couleur de la ford est: " + ford.color;
+    document.getElementById("monP").append(spanCreate);
+
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
